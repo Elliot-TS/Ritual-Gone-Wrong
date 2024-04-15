16b8b5fa6c2c08e4ff7ea3e967edb611548190b4
Sarah Bryan <bryansar@grinnell.edu>
2024-04-15 15:36:09 -0500

    Create EventClick.cs
    Made the basic event handler. We can adjust the methods as necessary.

---
+++ Assets/Scripts/EventClick.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventClick : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("pointer down");
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("pointer up");
+    }
+    
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("pointer clicked");
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("pointer entered something");
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("pointer exited something");
+    }
+
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

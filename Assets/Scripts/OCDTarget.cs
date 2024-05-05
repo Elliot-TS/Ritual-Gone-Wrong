f0016cd45c23c08260dfa8770d489115ce793e2b
Elliot-TS <elliotbb@believershome.com>
2024-05-04 20:42:54 -0500

    Baked lighting. Click on items to deactivate OCD

--- Assets/Scripts/OCDTarget.cs
+++ Assets/Scripts/OCDTarget.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,11 @@ using UnityEngine.EventSystems;
 
 public class OCDTarget : MonoBehaviour, IPointerClickHandler
 {
+    public bool On = false;
     private bool once = true;
     // Start is called before the first frame update
     void Start()
     {
-        Enable();
     }
 
     // Update is called once per frame
@@ -23,11 +24,13 @@ public class OCDTarget : MonoBehaviour, IPointerClickHandler
 
     public void Enable()
     {
+        On = true;
         gameObject.GetComponent<Highlight>().On = true;
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        On = false;
         gameObject.GetComponent<Highlight>().On = false;
     }
 }

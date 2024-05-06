14457638b5c35226caffa45a3cf328079d94ce47
Elliot-TS <elliotbb@believershome.com>
2024-05-06 09:44:57 -0500

    Fixed open door code.  Clicking on cabinets opens/closes them.

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -2,47 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.EventSystems;
 
-public class DoorOpen : MonoBehaviour
+public class DoorOpen : MonoBehaviour, IPointerClickHandler
 {
-    public GameObject door;
+    public float AngleOpen = 0;
+    public float AngleClose = 90;
+    public float speed = 3;
 
-    public float angleOpen;
-    public float angleClose;
-    public float speed;
+    public bool IsOpen = false;
+    private bool isOpenChange = false;
 
-    public bool opening;
-    public bool check;
+    private bool opening = false;
+    private bool closing = false;
 
     void Update()
     {
-        Vector3 currentAngle = door.transform.localEulerAngles;
-
-        if(opening)
-        {
-            if(angleOpen > angleClose)
-            {
-                if(Math.Abs(currentAngle.y) < Math.Abs(angleOpen))
-                {
-                    door.transform.localEulerAngles = Vector3.Lerp(currentAngle, new Vector3(currentAngle.x, angleOpen, currentAngle.z), speed * Time.deltaTime);
-                }
-            }
-
-            else
-            {
-                if(Math.Abs(currentAngle.y) < Math.Abs(angleOpen))
-                {
-                    door.transform.localEulerAngles = Vector3.Lerp(currentAngle, new Vector3(currentAngle.x, angleOpen, currentAngle.z), speed * Time.deltaTime);
-                }
-            }
+        if (isOpenChange != IsOpen) {
+            if (IsOpen) Open();
+            else Close();
         }
+        isOpenChange = IsOpen;
 
-        else
-        {
-            if(Math.Abs(currentAngle.y) > Math.Abs(angleClose))
-            {
-                door.transform.localEulerAngles = Vector3.Lerp(currentAngle, new Vector3(currentAngle.x, angleClose, currentAngle.z), speed * Time.deltaTime);
-            }
+        if (opening) {
+            openDoor();
         }
+        else if (closing) {
+            closeDoor();
+        }
+    }
+
+    private void openDoor() {
+        Vector3 currentAngle = transform.localEulerAngles;
+        transform.localEulerAngles = Vector3.Lerp(currentAngle, new Vector3(currentAngle.x, AngleOpen, currentAngle.z), speed * Time.deltaTime);
+        if (Math.Abs(currentAngle.y - AngleOpen) < 0.01) opening = false;
+    }
+
+    private void closeDoor() {
+        Vector3 currentAngle = transform.localEulerAngles;
+        transform.localEulerAngles = Vector3.Lerp(currentAngle, new Vector3(currentAngle.x, AngleClose, currentAngle.z), speed * Time.deltaTime);
+        if (Math.Abs(currentAngle.y - AngleClose) < 0.01) closing = false;
+    }
+
+    public void Open() {
+        IsOpen = true;
+        isOpenChange = IsOpen;
+        opening = true;
+        closing = false;
+        Debug.Log("Opening");
+    }
+
+    public void Close() {
+        IsOpen = false;
+        isOpenChange = IsOpen;
+        opening = false;
+        closing = true;
+        Debug.Log("Closing");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (IsOpen) Close();
+        else Open();
+        Debug.Log("Click");
     }
 }
